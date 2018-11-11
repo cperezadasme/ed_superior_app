88f711c029a81fb3f379f3b29ac6f20be650b17a
Constanza Pérez <cperezadasme@gmail.com>
2018-11-11 18:33:05 -0300

    Added filters endpoints

--- backend/visualizacion/urls.py
+++ backend/visualizacion/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'matricula/$', views.MatriulaAPIView.as_view()),
     url(r'titulados/$', views.TituladosAPIView.as_view()),
+    url(r'years/$', views.YearAPIView.as_view()),
+    url(r'instituion-classification-3/$', views.ClassificationLevel3APIView.as_view()),
+    url(r'area-knowledge/$', views.AreaKnowledgeAPIView.as_view()),
 ]
--- backend/visualizacion/views.py
+++ backend/visualizacion/views.py
@@ -1,5 +1,7 @@
 # from django.shortcuts import render
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import Matricula, Titulados
 from .serializers import MatriculaSerializer, TituladosSerializer
@@ -16,3 +18,23 @@ class TituladosAPIView(ListAPIView):
     queryset = Titulados.objects.all()
     serializer_class = TituladosSerializer
     filter_class = TituladosFilter
+
+
+class YearAPIView(APIView):
+
+    def get(self, request, format=None):
+        queryset = Matricula.objects.values_list('year', flat=True).distinct()
+        return Response(queryset)
+
+
+class ClassificationLevel3APIView(APIView):
+    def get(self, request, format=None):
+        queryset = Matricula.objects.values_list(
+            'institution_classification_level_3', flat=True).distinct()
+        return Response(queryset)
+
+
+class AreaKnowledgeAPIView(APIView):
+    def get(self, request, format=None):
+        queryset = Matricula.objects.values_list('area_of_knowledge', flat=True).distinct()
+        return Response(queryset)
