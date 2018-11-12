94435b765f1be7a5f861bf850971a0b8dfd72e22
Constanza Pérez <cperezadasme@gmail.com>
2018-11-11 21:39:02 -0300

    Added total titulados endpoint

--- backend/visualizacion/urls.py
+++ backend/visualizacion/urls.py
@@ -2,9 +2,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'matricula/$', views.MatriulaAPIView.as_view()),
-    url(r'titulados/$', views.TituladosAPIView.as_view()),
-    url(r'years/$', views.YearAPIView.as_view()),
-    url(r'instituion-classification-3/$', views.ClassificationLevel3APIView.as_view()),
-    url(r'area-knowledge/$', views.AreaKnowledgeAPIView.as_view()),
+    url(r'matricula/$', views.MatriulaAPIView.as_view(), name='matricula'),
+    url(r'titulados/$', views.TituladosAPIView.as_view(), name='titulados'),
+    url(r'years/$', views.YearAPIView.as_view(), name='years'),
+    url(
+        r'instituion-classification-3/$',
+        views.ClassificationLevel3APIView.as_view(),
+        name='classification-3',
+    ),
+    url(r'area-knowledge/$', views.AreaKnowledgeAPIView.as_view(), name='area-knowledge'),
+    url(r'region/$', views.RegionAPIView.as_view(), name='region'),
+    url(r'total-graduates/$', views.TotalTituladosAPIView.as_view(), name='total-graduates'),
 ]
--- backend/visualizacion/views.py
+++ backend/visualizacion/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from django.db.models import Sum
+from rest_framework.generics import ListAPIView, GenericAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -38,3 +39,40 @@ class AreaKnowledgeAPIView(APIView):
     def get(self, request, format=None):
         queryset = Matricula.objects.values_list('area_of_knowledge', flat=True).distinct()
         return Response(queryset)
+
+
+class RegionAPIView(APIView):
+    def get(self, request, format=None):
+        queryset = Matricula.objects.values_list('region', flat=True).distinct()
+        return Response(queryset)
+
+
+class TotalTituladosAPIView(ListAPIView):
+    queryset = Titulados.objects.all()
+    serializer_class = TituladosSerializer
+    filter_class = TituladosFilter
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.queryset)
+
+        graduates_by_institution_types = queryset.values(
+            'year', 'institution_classification_level_3',
+        ).annotate(total_titulados=Sum('total_graduates'))
+
+        institution_types = graduates_by_institution_types.values_list(
+            'institution_classification_level_3', flat=True).distinct()
+        years = graduates_by_institution_types.values_list(
+            'year', flat=True).distinct()
+
+        data = {}
+
+        for institution_type in institution_types:
+            data[institution_type] = {}
+            for year in years:
+                total = graduates_by_institution_types.get(
+                    institution_classification_level_3=institution_type, year=year,
+                ).get('total_titulados')
+
+                data[institution_type][year] = total
+
+        return Response(data)
